46704fc6b61ef2aea2b6e5cfc43c1cee2ad5e659
Administrator <Administrator@SKCS4171>
2017-10-02 14:11:03 +0800

    加入專案檔案。

---
+++ TDD_Katas_Plus_One/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TDD_Katas_Plus_One
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
