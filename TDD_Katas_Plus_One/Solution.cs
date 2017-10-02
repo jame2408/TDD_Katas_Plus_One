775bccf95ba464b068b368506109153825256f76
JamesWang <jame2408@gmail.com>
2017-10-02 16:07:45 +0800

    Refactoring Prod code.

--- TDD_Katas_Plus_One/Solution.cs
+++ TDD_Katas_Plus_One/Solution.cs
@@ -6,18 +6,18 @@
         {
             for (int i = digits.Length - 1; i >= 0; i--)
             {
-                digits[i] += 1;
-                if (digits[i] <= 9)
+                if (digits[i] < 9)
                 {
+                    digits[i] += 1;
                     return digits;
                 }
                 digits[i] = 0;
             }
 
-            var ints = new int[digits.Length + 1];
-            ints[0] = 1;
+            var newdigits = new int[digits.Length + 1];
+            newdigits[0] = 1;
 
-            return ints;
+            return newdigits;
         }
     }
 }
