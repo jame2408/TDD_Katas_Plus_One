09603bee16d19b069d0feedd601e25cac9c8a8f9
JamesWang <jame2408@gmail.com>
2017-10-02 14:43:38 +0800

    Add second test : Test_when_input_2_Should_be_3. (red)

--- TDD_Katas_Plus_One/TestSolution.cs
+++ TDD_Katas_Plus_One/TestSolution.cs
@@ -16,5 +16,17 @@ namespace TDD_Katas_Plus_One
 
             Assert.AreEqual(expected, actual);
         }
+
+        [Test]
+        public void Test_when_input_2_Should_be_3()
+        {
+            var digits = new[] { 2 };
+            var target = new Solution();
+            var expected = new[] { 3 };
+
+            var actual = target.PlusOne(digits);
+
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
