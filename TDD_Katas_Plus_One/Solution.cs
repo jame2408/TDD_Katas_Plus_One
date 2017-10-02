8a1a1814466f2c1aa82940346722fa55612e88a1
JamesWang <jame2408@gmail.com>
2017-10-02 14:35:28 +0800

    Add first test : Test_when_input_1_Should_be_2. (red)

---
+++ TDD_Katas_Plus_One/Solution.cs
@@ -0,0 +1,14 @@
+﻿namespace TDD_Katas_Plus_One
+{
+    class Solution
+    {
+        public Solution()
+        {
+        }
+
+        public int[] PlusOne(int[] digits)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ TDD_Katas_Plus_One/TestSolution.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace TDD_Katas_Plus_One
+{
+    [TestFixture]
+    public class TestSolution
+    {
+        [Test]
+        public void Test_when_input_1_Should_be_2()
+        {
+            var digits = new[] {1};
+            var target = new Solution();
+            var expected = new[] {2};
+
+            var actual = target.PlusOne(digits);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
