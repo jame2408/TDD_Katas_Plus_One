b11fcd0c79232cbf54d1b3c39c5a2d2420aa10f3
JamesWang <jame2408@gmail.com>
2017-10-02 15:04:27 +0800

    Refactoring Test case code.

--- TDD_Katas_Plus_One/TestSolution.cs
+++ TDD_Katas_Plus_One/TestSolution.cs
@@ -5,28 +5,31 @@ namespace TDD_Katas_Plus_One
     [TestFixture]
     public class TestSolution
     {
+        Solution target = new Solution();
+
         [Test]
         public void Test_when_input_1_Should_be_2()
         {
-            var digits = new[] {1};
-            var target = new Solution();
-            var expected = new[] {2};
+            var initialDigits = new[] { 1 };
+            var expected = new[] { 2 };
 
-            var actual = target.PlusOne(digits);
+            AssertPlusOne(expected, initialDigits);
+        }
 
-            Assert.AreEqual(expected, actual);
+        private void AssertPlusOne(int[] expected, int[] initialDigits)
+        {
+            Assert.AreEqual(
+                expected, 
+                target.PlusOne(initialDigits));
         }
 
         [Test]
         public void Test_when_input_2_Should_be_3()
         {
-            var digits = new[] { 2 };
-            var target = new Solution();
+            var initialDigits = new[] { 2 };
             var expected = new[] { 3 };
 
-            var actual = target.PlusOne(digits);
-
-            Assert.AreEqual(expected, actual);
+            AssertPlusOne(expected, initialDigits);
         }
     }
 }
