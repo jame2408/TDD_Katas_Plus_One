7e57aa5419a90483d6628fc4a65f7f498fc9053a
JamesWang <jame2408@gmail.com>
2017-10-02 15:28:03 +0800

    Pass test : Test_when_input_1_9_Should_be_2_0. (green)

--- TDD_Katas_Plus_One/Solution.cs
+++ TDD_Katas_Plus_One/Solution.cs
@@ -4,7 +4,15 @@
     {
         public int[] PlusOne(int[] digits)
         {
-            digits[digits.Length - 1] += 1;
+            for (int i = digits.Length - 1; i >= 0; i--)
+            {
+                digits[i] += 1;
+                if (digits[i] <= 9)
+                {
+                    return digits;
+                }
+                digits[i] = 0;
+            }
             return digits;
         }
     }
